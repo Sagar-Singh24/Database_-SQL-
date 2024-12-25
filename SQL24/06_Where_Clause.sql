f6023515155b6ec5ab668267ec0b58057318dfb0
Sagar Singh <sssssagarsingh24@gmail.com>
2024-12-26 00:27:42 +0530

    Add files via upload

---
+++ SQL24/06_Where_Clause.sql
@@ -0,0 +1,36 @@
+CREATE DATABASE colleges;
+
+USE colleges;
+
+CREATE TABLE student1(
+  rollno INT PRIMARY KEY,
+  name VARCHAR(50),
+  marks INT NOT NULL,
+  grade VARCHAR(1),
+  city VARCHAR (20)
+  );
+  
+INSERT INTO student1
+(rollno,name,marks,grade,city)
+VALUES
+(101, "Ram",78,"C","Pune"),
+(102, "Rohan",93,"A","Kolkata"),
+(103, "Shyam",85,"B","Mumbai"),
+(104, "Aman",96,"A","Patna"),
+(105, "Anil",12,"F","Delhi"),
+(106, "Anuj",82,"B","Pune");
+
+SELECT * FROM student1;
+
+SELECT *
+FROM student1
+WHERE marks > 80;
+
+SELECT *
+FROM student1
+WHERE city = "Patna";
+
+SELECT *
+FROM student1
+WHERE marks> 80 AND city = "Patna";
+
